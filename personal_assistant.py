1867e5e45d218849f30341cadcc3c46928399165
Efim <e.novoselov@edu.centraluniversity.ru>
2024-11-29 17:03:51 +0300

    Добавил функцию просмотра подробностей заметок и редактирование заметок

--- personal_assistant.py
+++ personal_assistant.py
@@ -2,6 +2,8 @@ from datetime import datetime
 import json
 from pprint import pprint
 
+from wheel.macosx_libfile import read_data
+
 
 class GreetPage:
     def tool_bars(self):
@@ -41,7 +43,7 @@ class Note:
         self.content = None
         self.timestamp = None
 
-
+    # Функция управления заметками
     def managing_notes(self):
         while True:
             print('Управление заметками:')
@@ -56,18 +58,20 @@ class Note:
             choice = input('Выберите действие: ')
 
             if choice == "1":
-                now = datetime.now()
-                self.timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
                 self.title = input('Введите название заметки: ')
                 self.content = input('Введите текст: ')
-
+                self.timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
                 self.create_note(self.title, self.content, self.timestamp)
             elif choice == "2":
                 self.viewing_notes()
             elif choice == "3":
-                pass
+                self.view_note_details()
             elif choice == "4":
-                pass
+                self.id = int(input('Введите id интересующей вас заметки: '))
+                self.title = input('Введите название заметки: ')
+                self.content = input('Введите текст: ')
+                self.timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+                self.editing_note(self.id, self.title, self.content, self.timestamp)
             elif choice == "5":
                 pass
             elif choice == "6":
@@ -78,44 +82,67 @@ class Note:
             else:
                 print("Неверный ввод. Пожалуйста, выберите номер от 1 до 7.")
 
-
+    # Функция создания заметок
     def create_note(self, title, content, timestamp):
-        #Достаем id следующей заметки
-        try:
-            with open('notes.json', 'r', encoding='utf-8') as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            data = []
-        except Exception as e:
-            print(f'Произошла неведомая ошибка {e}')
-            return
+        # Достаем id следующей заметки
+        data = self.read_notes()
 
         if data:
             id = int(data[-1].get('id', 1)) + 1
         else:
             id = 1
-            
-        #Добавляем новую заметку к data
+
+        # Добавляем новую заметку к data
         new_note = {'id': id,
                     'title': title,
                     'content': content,
                     'timestamp': timestamp}
         data.append(new_note)
 
-        #Грузим заметки в notes.json
-        with open('notes.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-        print('Заметка успешно добавлена в файл notes.json\n')
+        # Грузим заметки в notes.json
+        self.upload_notes(data)
 
-
+    # Функция просмотра заметок
     def viewing_notes(self):
+        data = self.read_notes()
+        pprint(data)
+
+    # Я не понял, че значит "Просмотр подробностей заметки", поэтому просто вывожу инфу о конкретной заметке
+    def view_note_details(self):
+        data = self.read_notes()
+        id = int(input('Введите id интересующей вас заметки: '))
+        if len(data) < id or id < 1:
+            print('Ячейки с таким id не существует')
+        else:
+            pprint(data[id - 1])
+
+    # Редактирование заметки
+    def editing_note(self, id, title, content, timestamp):
+        data = self.read_notes()
+        if 1 <= id <= len(data):
+            new_note = {'id': id,
+                        'title': title,
+                        'content': content,
+                        'timestamp': timestamp}
+            data[id - 1] = new_note
+            self.upload_notes(data)
+        else:
+            print('Ячейки с таким id не существует')
+
+    # Отдельная функция чтения заметок
+    def read_notes(self):
         try:
             with open('notes.json', 'r', encoding='utf-8') as file:
                 data = json.load(file)
         except FileNotFoundError:
             data = []
-        pprint(data)
+        return data
 
+    # Отдельная функция загрузки заметок в файл notes.json
+    def upload_notes(self, data):
+        with open('notes.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+        print('Заметка успешно добавлена в файл notes.json\n')
 
 
 ###Тесты
